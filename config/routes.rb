0e3628fdbda0f5481b98afb279935a184a9bea0f
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-05-29 19:06:03 +0900

    ログインしたユーザーが最初に訪れるページ「ダッシュボード」を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
-  get "up" => "rails/health#show", as: :rails_health_check
-  root 'home#index' #(仮)
-  # Defines the root path route ("/")
-  # root "posts#index"
+  devise_for :users # Devise関連のURL（/users/sign_inなど）
+
+  # ★★★ この行を追加 ★★★
+  # "/dashboard" というURLにアクセスしたら、DashboardControllerのindexアクションを実行してね、という意味
+  # `as: 'user_dashboard'` は、このURLに `user_dashboard_path` という名前（呼び名）をつける設定
+  get 'dashboard', to: 'dashboard#index', as: 'user_dashboard'
+
+  root 'home#index' # あなたのアプリの一番最初のページ（ランディングページ）
 end
